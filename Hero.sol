e44f22059bc02b3fd3896388cd67ad9d1c148ef4
stmangame <96165267+stmangame@users.noreply.github.com>
2022-02-25 17:24:45 +0700

    Add files via upload

--- Hero.sol
+++ Hero.sol
@@ -7,13 +7,19 @@ import "openzeppelin-solidity/contracts/access/Ownable.sol";
 import "openzeppelin-solidity/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/utils/SafeERC20.sol";
 import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/access/AccessControlEnumerable.sol";
+import "./IHero.sol";
 
-contract Hero is ERC721Enumerable, Ownable {
+contract Hero is ERC721Enumerable, Ownable,AccessControlEnumerable,IHero {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdTracker;
     string private _url;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    event Mint(address to,uint256 hero_type,uint256 tokenid);
 
     constructor() ERC721("Stickman Hero", "Hero") {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
     function _baseURI()
@@ -25,11 +31,12 @@ contract Hero is ERC721Enumerable, Ownable {
         return _url;
     }
 
-    function mint(address _to) external onlyOwner returns (uint256) {
-        require(balanceOf(_to)==0,"One address only have one Hero");
+    function mint(address to,uint256 hero_type) external override returns (uint256) {
+        require(owner() == _msgSender()||hasRole(MINTER_ROLE,_msgSender()), "Caller is not a minter");
         _tokenIdTracker.increment();
         uint256 token_id = _tokenIdTracker.current();
-        _mint(_to, token_id);
+        _mint(to, token_id);
+        emit Mint(to,hero_type,token_id);
         return token_id;
     }
 
@@ -42,32 +49,17 @@ contract Hero is ERC721Enumerable, Ownable {
             ids[i]=tokenOfOwnerByIndex(owner,i);
         }
         return (ids);
-    } 
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
-        require(balanceOf(to)==0,"One address only have one Hero");
-        _transfer(from, to, tokenId);
-    }
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory _data
-    ) public virtual override {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
-        require(balanceOf(to)==0,"One address only have one Hero");
-        _safeTransfer(from, to, tokenId, _data);
     }
 
     function setBaseUrl(string memory _newUrl) public onlyOwner
     {
         _url=_newUrl;
     }
+
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Enumerable, AccessControlEnumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
 
 }
